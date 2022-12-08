ff68c2d75719da23f744df58221b47597cec9265
user <joshmanuel2016@gmail.com>
2022-12-08 17:36:51 -0500

    fix: add candidate fonts for marquees -> pending approval

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -24,7 +24,7 @@ const MarqueeContainer = styled.section`
     color: #808080 !important;
     font-size: 7rem;
     font-weight: 100;
-    font-family: "Roboto Slab", sans-serif;
+    font-family: "Gilda Display", sans-serif;
     text-transform: uppercase;
     letter-spacing: 0.8rem;
   }
@@ -41,7 +41,6 @@ const MarqueeContainer = styled.section`
       letter-spacing: 0rem;
     }
   }
- 
 `;
 
 const AboutSection = () => {
