a9ac906b2e8a8d8c3b9e2437f51e169a4737fd64
user <joshmanuel2016@gmail.com>
2022-12-10 16:42:23 -0500

    fix(ProjectSection): make bottom margin for heading larger

--- src/components/ProjectSection/ProjectSection.js
+++ src/components/ProjectSection/ProjectSection.js
@@ -32,6 +32,7 @@ const ProjectSect = styled.section`
     letter-spacing: 1.5rem;
     line-height: 5rem;
     margin-top: 200px;
+    margin-bottom: 50px;
   }
 
   @media only screen and (max-width: 768px) {
